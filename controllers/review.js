dea38ccc3e6ed48722e8317c7ac9e4e4b1d71356
Ronak prasad <ronakprasad495@gmail.com>
2025-02-28 20:18:59 +0530

    review redirect solved

--- controllers/review.js
+++ controllers/review.js
@@ -18,9 +18,11 @@ module.exports.createReview = async (req, res) => {
 
 module.exports.destroyReview = async (req, res) => {
     let {id, reviewId} = req.params;
-  
+    console.log("listingid=", id);
+    console.log("reviewID=",reviewId);
     await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}})
+    console.log(id);
     await Review.findByIdAndDelete(reviewId);
     req.flash("success", "Review Deleted!");
-    res.redirect("/listings/${id}"); 
+    res.redirect(`/listings/${id}`); 
 };
